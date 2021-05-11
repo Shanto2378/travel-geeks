c5e91e94aadabd5a66aee96df9bf79507d59dad6
neshatmouri31 <neshatmouri7@gmail.com>
2021-05-11 16:51:13 +0600

    admin correction and backend connect

--- SignUp/admin_backend.php
+++ SignUp/admin_backend.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
  include "database_connection.php";
  $db = mysqli_select_db($conn , 'travel-geeks');
  if(isset($_POST['asignin'])){
@@ -6,15 +7,15 @@
      $apassword = $_POST['password'];
 $sql = "select * from admin where email = '$aemail' and password = 'apassword'";
 $query = mysqli_query($conn,$sql);
-$row = mysqli_num_row(){
-    if($row == 1){
+    if($aemail == "travel@gmail.com" && $apassword == "123"){
         echo "login successful";
+        $_SESSION['useremail']=$aemail;
         header("location: index.php");
     }
     else{
         header("location: admin_login.php");
     }
-}
+
 
 
  }
