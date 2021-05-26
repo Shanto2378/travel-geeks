00978aed8435647a5edefec4e30fdc03723c4423
Shanto2378 <hasibul.shanto1@northsouth.edu>
2021-05-27 02:13:28 +0600

    Tourist page fixed image and text fixed

--- SignUp/Tourist Spots.php
+++ SignUp/Tourist Spots.php
@@ -102,7 +102,7 @@ include('header.php');
                         <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
                             <div class="u-container-layout u-container-layout-2">
                                 <h2 class="u-text u-text-1">Sundarbans</h2>
-                                <p class="u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                <p class="u-text u-text-2">Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh's division of Khulna.</p>
                             </div>
                         </div>
                     </div>
@@ -113,7 +113,7 @@ include('header.php');
     <section class="u-align-center-xs u-clearfix u-grey-15 u-section-3" id="sec-2c6f">
         <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
             <h1 class="u-align-center u-text u-text-1">Best places to go in Sundarbans</h1>
-            <p class="u-align-center u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+            <p class="u-align-center u-text u-text-2"></p>
             <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
                 <div class="u-gutter-0 u-layout">
                     <div class="u-layout-col">
@@ -126,7 +126,7 @@ include('header.php');
                                     <div class="u-container-layout u-valign-middle u-container-layout-2">
                                         <h4 class="u-align-center u-text u-text-3">Hiron Point Bangladesh<br>
                                         </h4>
-                                        <p class="u-align-center u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-4">Hiron Point, a protected sanctuary in the southern part of the Sundarbans. Another name is Nilkamol. Located on the western bank of the river Kunga, in the Khulna Range. It is a UNESCO World Heritage Site under Sundarbans.</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-20 u-image-2" data-image-width="540" data-image-height="360">
@@ -140,7 +140,7 @@ include('header.php');
                                     <div class="u-container-layout u-valign-middle u-container-layout-4">
                                         <h4 class="u-align-center u-text u-text-5">Sundarban National Park<br>
                                         </h4>
-                                        <p class="u-align-center u-text u-text-6">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-6">The Sundarbans National Park is a national park, tiger reserve, and biosphere reserve in West Bengal, India. It is part of the Sundarbans on the Ganges Delta, and adjacent to the Sundarban Reserve Forest in Bangladesh.</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-size-20 u-image-3" data-image-width="480" data-image-height="720">
@@ -149,7 +149,7 @@ include('header.php');
                                 <div class="u-container-style u-layout-cell u-right-cell u-size-20 u-white u-layout-cell-6">
                                     <div class="u-container-layout u-valign-middle u-container-layout-6">
                                         <h4 class="u-align-center u-text u-text-7">Sajnekhali Tiger Reserve</h4>
-                                        <p class="u-align-center u-text u-text-8">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-8">Sajnakhali Wildlife Sanctuary is a 362 km2 area in the northern part of the Sundarbans delta in South 24 Parganas district, West Bengal, India. The area is mainly mangrove scrub, forest and swamp. It was set up as a sanctuary in 1976. It is home to a rich population of different species of wildlife, such as water fowl, heron, pelican, spotted deer, rhesus macaques, wild boar, tigers, water monitor lizards, fishing cats, otters, Olive ridley turtle, crocodiles, Batagur terrapins, and migratory birds.</p>
                                     </div>
                                 </div>
                             </div>
@@ -173,7 +173,7 @@ include('header.php');
                         <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
                             <div class="u-container-layout u-container-layout-2">
                                 <h2 class="u-text u-text-1">Dhaka</h2>
-                                <p class="u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                <p class="u-text u-text-2">Dhaka formerly known as Dacca, is the capital and the largest city of Bangladesh. It is the ninth-largest and the sixth-most densely populous city in the world with, as of 2011, a population of 8.9 million residents within the city limits, and a population of over 21 million residents in the Greater Dhaka Area. Dhaka is the economic, political, and cultural center of Bangladesh, and is one of the major cities in South Asia, the largest city in Eastern South Asia and among the Bay of Bengal countries; and one of the largest cities among OIC countries. As part of the Bengal plain, the city is bounded by the Buriganga River, Turag River, Dhaleshwari River and Shitalakshya River.</p>
                             </div>
                         </div>
                     </div>
@@ -184,7 +184,7 @@ include('header.php');
     <section class="u-align-center-xs u-clearfix u-grey-15 u-section-5" id="carousel_e69e">
         <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
             <h1 class="u-align-center u-text u-text-1">Best places to go in Dhaka City</h1>
-            <p class="u-align-center u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+            <p class="u-align-center u-text u-text-2"></p>
             <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
                 <div class="u-gutter-0 u-layout">
                     <div class="u-layout-col">
@@ -196,7 +196,7 @@ include('header.php');
                                 <div class="u-container-style u-layout-cell u-size-20 u-white u-layout-cell-2">
                                     <div class="u-container-layout u-valign-middle u-container-layout-2">
                                         <h4 class="u-align-center u-text u-text-3">Bangladesh National Parliament</h4>
-                                        <p class="u-align-center u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-4">Jatiya Sangsad Bhaban or National Parliament House, is the house of the Parliament of Bangladesh, located at Sher-e-Bangla Nagar in the Bangladeshi capital of Dhaka. Designed while the country was still part of Pakistan by architect Louis Kahn, the complex is one of the largest legislative complexes in the world, comprising 200 acres.</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-20 u-image-2" data-image-width="539" data-image-height="360">
@@ -208,8 +208,8 @@ include('header.php');
                             <div class="u-layout-row">
                                 <div class="u-container-style u-layout-cell u-left-cell u-size-20 u-white u-layout-cell-4">
                                     <div class="u-container-layout u-valign-middle u-container-layout-4">
-                                        <h4 class="u-align-center u-text u-text-5">Lalbag Fort</h4>
-                                        <p class="u-align-center u-text u-text-6">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <h4 class="u-align-center u-text u-text-5">Lalbagh Fort</h4>
+                                        <p class="u-align-center u-text u-text-6">Lalbagh Fort (also Fort Aurangabad) is an incomplete 17th-century Mughal fort complex that stands before the Buriganga River in the southwestern part of Dhaka, Bangladesh.[1] The construction was started in 1678 AD by Mughal Subahdar Muhammad Azam Shah, who was son of Emperor Aurangzeb and later emperor himself. His successor, Shaista Khan, did not continue the work, though he stayed in Dhaka up to 1688.</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-size-20 u-image-3" data-image-width="516" data-image-height="360">
@@ -219,7 +219,7 @@ include('header.php');
                                     <div class="u-container-layout u-valign-middle u-container-layout-6">
                                         <h4 class="u-align-center u-text u-text-7">Dhakeshwari Mandir<br>
                                         </h4>
-                                        <p class="u-align-center u-text u-text-8">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-8">Dhakeshwari National Temple is a Hindu temple in Dhaka, Bangladesh. It is state-owned, giving it the distinction of being Bangladesh's 'National Temple'. The name "Dhakeshwari" (ঢাকেশ্বরী Ðhakeshshori) means "Goddess of Dhaka". Since the destruction of Ramna Kali Mandir in 1971 by the Pakistan Army during the Bangladesh Liberation War, the Dhakeshwari Temple has assumed status as the most important Hindu place of worship in Bangladesh. It is also the largest Hindu temple in Bangladesh. This temple is part of the famous Shakti Peethas in Indian Subcontinent. Here the gem of sati's crown had fallen.</p>
                                     </div>
                                 </div>
                             </div>
@@ -243,7 +243,7 @@ include('header.php');
                         <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
                             <div class="u-container-layout u-container-layout-2">
                                 <h2 class="u-text u-text-1">Cox's Bazar</h2>
-                                <p class="u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                <p class="u-text u-text-2">Cox's Bazar, Bangladesh, is the longest natural sea beach in the world.</p>
                             </div>
                         </div>
                     </div>
@@ -254,7 +254,7 @@ include('header.php');
     <section class="u-align-center-xs u-clearfix u-grey-15 u-section-7" id="carousel_1378">
         <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
             <h1 class="u-align-center u-text u-text-1">Best places to go in Chittagong City</h1>
-            <p class="u-align-center u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+            <p class="u-align-center u-text u-text-2"></p>
             <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
                 <div class="u-gutter-0 u-layout">
                     <div class="u-layout-col">
@@ -266,7 +266,7 @@ include('header.php');
                                 <div class="u-container-style u-layout-cell u-size-20 u-white u-layout-cell-2">
                                     <div class="u-container-layout u-valign-middle u-container-layout-2">
                                         <h4 class="u-align-center u-text u-text-3">Himchari National Park</h4>
-                                        <p class="u-align-center u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-4">Himchari National Park is a major national park and nature reserve in Bangladesh. The park is located at Ramu and Cox's Bazar Sadar Upazila, Cox's Bazar District, in the southeast region of the country. It is located mainly on the hills and is adjoining to Bay of Bengal to the west. Himchari National Park covers approximately 1,729 ha (17.29 km2) of mixed evergreen forests Biome. It was declared a protected area in 1980</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-20 u-image-2" data-image-width="464" data-image-height="260">
@@ -279,7 +279,7 @@ include('header.php');
                                 <div class="u-container-style u-layout-cell u-left-cell u-size-20 u-white u-layout-cell-4">
                                     <div class="u-container-layout u-valign-middle u-container-layout-4">
                                         <h4 class="u-align-center u-text u-text-5">Cox's Bazar Beach</h4>
-                                        <p class="u-align-center u-text u-text-6">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-6">Cox's Bazar, Bangladesh, is the longest natural sea beach in the world running 150 kilometres (93 mil) and 3rd longest beach after Ninety Mile Beach of Australia and Praia do Cassino of Brazil. It is the top tourist destination of Bangladesh</p>
                                     </div>
                                 </div>
                                 <div class="u-align-center u-container-style u-image u-layout-cell u-size-20 u-image-3" data-image-width="464" data-image-height="260">
@@ -288,7 +288,7 @@ include('header.php');
                                 <div class="u-container-style u-layout-cell u-right-cell u-size-20 u-white u-layout-cell-6">
                                     <div class="u-container-layout u-valign-middle u-container-layout-6">
                                         <h4 class="u-align-center u-text u-text-7">Radiant fish world restaurant</h4>
-                                        <p class="u-align-center u-text u-text-8">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                        <p class="u-align-center u-text u-text-8">Radiant Fish world is Bangladesh first sea aquarium that is located in Cox's Bazar. There is also a fish museum.</p>
                                     </div>
                                 </div>
                             </div>
